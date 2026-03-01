8473b62ed72342d0de93a752ee86809f577bd401
Ankush Gupta <ankushgupta7634@gmail.com>
2026-03-01 11:27:17 +0530

    Time: 43 ms (27.4%), Space: 46.1 MB (95.26%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,21 +1,95 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        int largest=0;
-        int smallest=0;
+    private int [] prevSmallestel(int nums[]){
+        Stack<Integer> st= new Stack<>();
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=-1;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+     private int [] nextSmallestel(int nums[]){
+        Stack<Integer> st= new Stack<>();
+        int ans[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=nums.length;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+     private int [] nextGreaterel(int nums[]){
+        Stack<Integer> st= new Stack<>();
+        int ans[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=nums.length;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+     private int [] prevGreaterel(int nums[]){
+        Stack<Integer> st= new Stack<>();
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=-1;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    private long sumSubaaraymin(int nums[]){
+        int n=nums.length;
+        int nse[]=nextSmallestel(nums);
+        int pse[]=prevSmallestel(nums);
         long sum=0;
         for(int i=0;i<nums.length;i++){
-          
-            smallest=nums[i];
-            largest=nums[i];
-            for(int j=i;j<nums.length;j++){
-                smallest=Math.min(smallest,nums[j]);
-                largest=Math.max(largest,nums[j]);
-                int range=largest-smallest;
-                sum=sum + range;
-            }
-           
+            int left=i-pse[i];
+            int right=nse[i]-i;
+           long freq=1l*left * right;
+           sum+=freq * nums[i];
         }
-
-     return sum;
+        return sum;
+    }
+    private long sumSubaaraymax(int nums[]){
+        int n=nums.length;
+        int nge[]=nextGreaterel(nums);
+        int pge[]=prevGreaterel(nums);
+        long sum=0;
+        for(int i=0;i<nums.length;i++){
+            int left=i-pge[i];
+            int right=nge[i]-i;
+           long freq=1l*left * right;
+           sum+=freq * nums[i];
+        }
+        return sum;
+    }
+    public long subArrayRanges(int[] nums) {
+        return sumSubaaraymax(nums) - sumSubaaraymin(nums);
     }
 }
