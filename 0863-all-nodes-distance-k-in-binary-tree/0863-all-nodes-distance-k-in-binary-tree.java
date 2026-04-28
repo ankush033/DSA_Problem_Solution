9d29809eac5045afa04d35d5badf8c1156bfac61
Ankush Gupta <ankushgupta7634@gmail.com>
2026-04-28 20:25:32 +0530

    Time: 14 ms (22.97%), Space: 44 MB (74.23%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void markparent(TreeNode root,Map<TreeNode ,TreeNode> parent_track,TreeNode target){
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode current=q.poll();
+            if(current.left!=null){
+                parent_track.put(current.left,current);
+                q.offer(current.left);
+            }
+            if(current.right!=null){
+                parent_track.put(current.right,current);
+                q.offer(current.right);
+            }
+        }
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode,TreeNode > parent_track=new HashMap<>();
+        markparent(root,parent_track,root);
+        Map<TreeNode ,Boolean> visited=new HashMap<>();
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.offer(target);
+        visited.put(target,true);
+        int curr_level=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            if(curr_level==k) break;
+            curr_level++;
+            for(int i=0;i<size;i++){
+                TreeNode current=q.poll();
+                if(current.left!=null && visited.get(current.left)==null){
+                    q.offer(current.left);
+                    visited.put(current.left,true);
+                }
+                 if(current.right!=null && visited.get(current.right)==null){
+                    q.offer(current.right);
+                    visited.put(current.right,true);
+                 }
+                    if(parent_track.get(current)!=null && visited.get(parent_track.get(current))==null){
+                        q.offer(parent_track.get(current));
+                        visited.put(parent_track.get(current),true);
+                    }
+                }
+            }
+        
+        List<Integer> result=new ArrayList<>();
+        while(!q.isEmpty()){
+            TreeNode current=q.poll();
+            result.add(current.val);
+        }
+        return result;
+    }
+}
