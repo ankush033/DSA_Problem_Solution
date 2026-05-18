c1593222d24c707b9cb3dbbfd51a8aa89c73a32e
Ankush Gupta <ankushgupta7634@gmail.com>
2026-05-18 09:09:22 +0530

    Time: 1 ms (78.53%), Space: 43.7 MB (61.79%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,30 +15,67 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> list=new  ArrayList<>();
-        if(root==null){
-            return list;
-        }
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        boolean lefttoright=true;
-        while(!q.isEmpty()){
-            int size=q.size();
-            LinkedList<Integer> level=new LinkedList<>();
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                if(lefttoright){
-                    level.addLast(node.val);
-                }else{
-                    level.addFirst(node.val);
-                }
-                if(node.left!=null) q.offer(node.left);
-                if(node.right!=null) q.offer(node.right);
-            }
-            list.add(level);
-            lefttoright=!lefttoright;
-        }
+    ArrayList<List<Integer> >list=new ArrayList<>();
+    Queue<TreeNode> q=new LinkedList<>();
+    if(root==null){
+        return list;
+    }
+   boolean flag=true;
+   q.add(root);
+   while(!q.isEmpty()){
+    int size=q.size();
+    LinkedList<Integer> level=new LinkedList<>();
+    for(int i=0;i<size;i++){
+         TreeNode node=q.remove();
+         if(flag){
+            level.addLast(node.val);
+         } else{
+            level.addFirst(node.val);
+         }
+         if(node.left!=null){
+            q.add(node.left);
+         }
+         if(node.right!=null){
+            q.add(node.right);
+         }
+    }
+    list.add(level);
+    flag=!flag;
+  
 
-return list;
+   }
+   return list;
+
+
+
+
+
     }
 }
+//         List<List<Integer>> list=new  ArrayList<>();
+//         if(root==null){
+//             return list;
+//         }
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.offer(root);
+//         boolean lefttoright=true;
+//         while(!q.isEmpty()){
+//             int size=q.size();
+//             LinkedList<Integer> level=new LinkedList<>();
+//             for(int i=0;i<size;i++){
+//                 TreeNode node=q.poll();
+//                 if(lefttoright){
+//                     level.addLast(node.val);
+//                 }else{
+//                     level.addFirst(node.val);
+//                 }
+//                 if(node.left!=null) q.offer(node.left);
+//                 if(node.right!=null) q.offer(node.right);
+//             }
+//             list.add(level);
+//             lefttoright=!lefttoright;
+//         }
+
+// return list;
+//     }
+// }
