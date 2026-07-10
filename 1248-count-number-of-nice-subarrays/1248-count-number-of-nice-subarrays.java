591e22a9b0ca0ebbe5a2bcb3b11bcbc6f2d11bb7
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-11 00:36:56 +0530

    Time: 11 ms (83.66%), Space: 59.8 MB (27.27%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,23 +1,89 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-       return atmost(nums,k)-atmost(nums,k-1);
-        
+        return atmost(nums,k)-atmost(nums,k-1);
     }
-    private int atmost(int nums[],int k){
-        if(k<0){
+       private int atmost(int nums[],int k){
+         if(k<0){
             return 0;
-        }
-        int left=0;
-        int sum=0;
-        int count=0;
-        for(int right=0;right<nums.length;right++){
+
+         }
+         int left=0;
+         int count=0;
+         int sum=0;
+         for(int right=0;right<nums.length;right++){
             sum+=nums[right]%2;
-           while(sum > k){
-            sum-=nums[left]%2;
-            left++;
-           }
-           count +=right-left+1;
-        }
-        return count;
-    }
+            while(sum>k){
+                sum-=nums[left]%2;
+                left++;
+
+            }
+            count+=right-left+1;
+
+         }
+         return count;
+       }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        return atmost(nums,k)-atmost(nums,k-1);
+        
+//     }
+//     private int atmost(int nums[],int k){
+//         if(k<0){
+//             return 0;
+//         }
+//         int left=0;
+//         int sum=0;
+//         int count=0;
+//         for(int right=0;right<nums.length;right++){
+//             sum+=nums[right]%2;
+//            while(sum > k){
+//             sum-=nums[left]%2;
+//             left++;
+//            }
+//            count +=right-left+1;
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
