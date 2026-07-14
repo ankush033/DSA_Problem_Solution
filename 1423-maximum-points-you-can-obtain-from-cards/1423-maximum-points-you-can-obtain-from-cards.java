09bb3d6477e23e7d1f5bef1ec987dd5e7aa79815
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-14 20:14:56 +0530

    Time: 3 ms (32.83%), Space: 61.7 MB (53.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -2,64 +2,69 @@ class Solution {
     public int maxScore(int[] cardPoints, int k) {
     
      
-      int leftsum=0;
-
-      for(int i=0;i<k;i++){
-          leftsum+=cardPoints[i];
-      }
-      int maxiSum=leftsum;
-      int right=cardPoints.length-1;
-      int rightsum=0;
-      for(int left=k-1;left>=0;left--){
-        
-        rightsum+=cardPoints[right];
-        leftsum-=cardPoints[left];
-        maxiSum=Math.max(maxiSum,leftsum+rightsum);
-        right--;
-      }
-  return maxiSum;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         int maxsum=0;
-//         int leftSum=0;
-//         int rightSum=0;
-//         int n=cardPoints.length;
-//         for(int left=0;left<k;left++){
-//             leftSum+=cardPoints[left];
+//      int total=0;
+//      int n=cardPoints.length;
+//      for(int i=0;i<n;i++){
+//         total+=cardPoints[i];
+//      }
+//       if (k == n) {
+//             return total;
 //         }
-//         maxsum=leftSum;
-//         int right=n-1;
-//         for(int left=k-1;left>=0;left--){
-//             leftSum-=cardPoints[left];
-//             rightSum+=cardPoints[right];
-//             right--;
-//             maxsum=Math.max(maxsum,leftSum + rightSum);
-//         }
-
-        
-
-//      return maxsum;
-        
-
+//      int windowSize=n-k;// remaining window because we had taken there so there remaain for 4 window length;ab ismai hum minimum window ka sum lenge tab hume 3 point ya card max milen ge
+//      int minimumWindowSum =0;
+//      for(int i=0;i<windowSize;i++){
+//           minimumWindowSum+=cardPoints[i];
+//      }
+//       int minimumSum=minimumWindowSum;
+//       for(int right=k;right<n;right++){
+//        int  left=right-k;
+//        minimumWindowSum-=cardPoints[left];
+//        minimumWindowSum+=cardPoints[right];
+//        minimumSum=Math.min(minimumSum,minimumWindowSum);
+//       }
+//       return total-minimumSum;
 //     }
-
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int maxsum=0;
+        int leftSum=0;
+        int rightSum=0;
+        int n=cardPoints.length;
+        for(int left=0;left<k;left++){
+            leftSum+=cardPoints[left];
+        }
+        maxsum=leftSum;
+        int right=n-1;
+        for(int left=k-1;left>=0;left--){
+            leftSum-=cardPoints[left];
+            rightSum+=cardPoints[right];
+            right--;
+            maxsum=Math.max(maxsum,leftSum + rightSum);
+        }
+
+        
+
+     return maxsum;
+        
+
+    }
+
+}
