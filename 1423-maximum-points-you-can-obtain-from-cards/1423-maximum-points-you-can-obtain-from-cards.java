b024f830c65a1b5d0611f56618ef85911146387a
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-14 19:58:52 +0530

    Time: 2 ms (99.7%), Space: 62.1 MB (17.26%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,26 +1,65 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int maxsum=0;
-        int leftSum=0;
-        int rightSum=0;
-        int n=cardPoints.length;
-        for(int left=0;left<k;left++){
-            leftSum+=cardPoints[left];
-        }
-        maxsum=leftSum;
-        int right=n-1;
-        for(int left=k-1;left>=0;left--){
-            leftSum-=cardPoints[left];
-            rightSum+=cardPoints[right];
-            right--;
-            maxsum=Math.max(maxsum,leftSum + rightSum);
-        }
+    
+     
+      int leftsum=0;
 
+      for(int i=0;i<k;i++){
+          leftsum+=cardPoints[i];
+      }
+      int maxiSum=leftsum;
+      int right=cardPoints.length-1;
+      int rightsum=0;
+      for(int left=k-1;left>=0;left--){
         
-
-     return maxsum;
-        
-
+        rightsum+=cardPoints[right];
+        leftsum-=cardPoints[left];
+        maxiSum=Math.max(maxiSum,leftsum+rightsum);
+        right--;
+      }
+  return maxiSum;
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int maxsum=0;
+//         int leftSum=0;
+//         int rightSum=0;
+//         int n=cardPoints.length;
+//         for(int left=0;left<k;left++){
+//             leftSum+=cardPoints[left];
+//         }
+//         maxsum=leftSum;
+//         int right=n-1;
+//         for(int left=k-1;left>=0;left--){
+//             leftSum-=cardPoints[left];
+//             rightSum+=cardPoints[right];
+//             right--;
+//             maxsum=Math.max(maxsum,leftSum + rightSum);
+//         }
+
+        
+
+//      return maxsum;
+        
+
+//     }
+
+// }
