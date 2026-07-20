50996da7c1af843099dfb8323cf2ac0cdb72a530
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-20 17:20:38 +0530

    Time: 6 ms (99.49%), Space: 49.6 MB (59.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,47 @@
 class Solution {
-    private String generateKey(String str){
-        int count[]=new int[26];
-        for(char ch : str.toCharArray()){
-            count[ch-'a']+=1;
-        }
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<26;i++){
-            sb.append('#');
-            sb.append(count[i]);
-        }
-        return sb.toString();
-    }
-    public List<List<String>> groupAnagrams(String[] strs) {
-        
-         HashMap<String,ArrayList<String>> map = new HashMap<>();
 
-         for(int i=0;i<strs.length;i++){
-          String key=generateKey(strs[i]);
-         
-         if(!map.containsKey(key)){
-            map.put(key,new ArrayList<>());
-         }
-         map.get(key).add(strs[i]);
-         }
-         return new ArrayList<>(map.values());
-    }
+public List<List<String>> groupAnagrams(String[] strs) {
+  HashMap<String,List<String>> map=new HashMap<>();
+  for(String words :strs){
+    char arr[]=words.toCharArray();
+      Arrays.sort(arr);
+      String key=new String(arr);
+      if(!map.containsKey(key)){
+        map.put(key,new ArrayList<>());
+      }
+       map.get(key).add(words);
+  }
+ return new ArrayList<>(map.values());
+
+
+
+
 }
+}
+//     private String generateKey(String str){
+//         int count[]=new int[26];
+//         for(char ch : str.toCharArray()){
+//             count[ch-'a']+=1;
+//         }
+//         StringBuilder sb=new StringBuilder();
+//         for(int i=0;i<26;i++){
+//             sb.append('#');
+//             sb.append(count[i]);
+//         }
+//         return sb.toString();
+//     }
+//     public List<List<String>> groupAnagrams(String[] strs) {
+        
+//          HashMap<String,ArrayList<String>> map = new HashMap<>();
+
+//          for(int i=0;i<strs.length;i++){
+//           String key=generateKey(strs[i]);
+         
+//          if(!map.containsKey(key)){
+//             map.put(key,new ArrayList<>());
+//          }
+//          map.get(key).add(strs[i]);
+//          }
+//          return new ArrayList<>(map.values());
+//     }
+// }
