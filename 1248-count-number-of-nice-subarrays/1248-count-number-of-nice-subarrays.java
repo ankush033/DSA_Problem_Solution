91f68fd25bd5aae5d403c7e1de64a10da5562ec4
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-23 16:47:13 +0530

    Time: 42 ms (23.7%), Space: 56.8 MB (78.08%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,29 +1,45 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
-    }
-       private int atmost(int nums[],int k){
-         if(k<0){
-            return 0;
-
-         }
-         int left=0;
-         int count=0;
-         int sum=0;
-         for(int right=0;right<nums.length;right++){
-            sum+=nums[right]%2;
-            while(sum>k){
-                sum-=nums[left]%2;
-                left++;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int OddCount=0;
+        int ans=0;
+        for(int num : nums){
+            if(num % 2 !=0){
+                OddCount++;
+            }
+                    ans+=map.getOrDefault(OddCount-k,0);
+                
+                map.put(OddCount,map.getOrDefault(OddCount,0)+1);
 
             }
-            count+=right-left+1;
-
-         }
-         return count;
-       }
-    
+      
+        
+              return ans;
+    }
 }
+//        private int atmost(int nums[],int k){
+//          if(k<0){
+//             return 0;
+
+//          }
+//          int left=0;
+//          int count=0;
+//          int sum=0;
+//          for(int right=0;right<nums.length;right++){
+//             sum+=nums[right]%2;
+//             while(sum>k){
+//                 sum-=nums[left]%2;
+//                 left++;
+
+//             }
+//             count+=right-left+1;
+
+//          }
+//          return count;
+//        }
+    
+// }
 
 
 
