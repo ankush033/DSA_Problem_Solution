3ab07b8d7e593c832ba12f4821c6d91791553750
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-26 09:42:05 +0530

    Time: 6 ms (85.59%), Space: 46.4 MB (32.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,65 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int num : nums){
-            int idx=Collections.binarySearch(list,num);
-            if(idx <0){
-                idx=-(idx + 1);
-            }
-            if(idx==list.size()){
-                list.add(num);
-            }else{
-                list.set(idx,num);
-            }
+
+ArrayList<Integer> list=new ArrayList<>();
+for(int num : nums){
+    int left=0;
+    int right=list.size();
+ 
+    while(left<right){
+           int mid=left +(right-left)/2;
+        if(list.get(mid)<num){
+            left=mid+1;
+
+        }else{
+            right=mid;
         }
-        return list.size();
+    }
+        if(list.size()==left){
+            list.add(num);
+        }else{
+            list.set(left,num);
+        }
+    
+}
+return list.size();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+//         ArrayList<Integer> list=new ArrayList<>();
+//         for(int num : nums){
+//             int idx=Collections.binarySearch(list,num);
+//             if(idx <0){
+//                 idx=-(idx + 1);
+//             }
+//             if(idx==list.size()){
+//                 list.add(num);
+//             }else{
+//                 list.set(idx,num);
+//             }
+//         }
+//         return list.size();
+//     }
+// }
