5c0ae5866eb98de64f1136a0ae140eeff7b0bb2c
Ankush Gupta <ankushgupta7634@gmail.com>
2026-07-26 18:37:05 +0530

    Time: 57 ms (84.09%), Space: 76 MB (96.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,39 +1,76 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-      int MaxArea=0;
-      int nsr[]=new int[heights.length];
-      int nsl[]=new int[heights.length];
-      Stack<Integer> s=new Stack<>();
-      for(int i=heights.length-1;i>=0;i--){
-        while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
-            s.pop();
+      Stack<Integer> st=new Stack<>();
+      int maxArea=0;
+      int n=heights.length;
+      for(int i=0;i<=n;i++){
+        while(!st.isEmpty() && (i==n || heights[st.peek()]>=heights[i])){
+            int height=heights[st.pop()];
+            int width;
+            if(st.isEmpty()){
+                width=i;
+            }else{
+                width=i-st.peek()-1;
+            }
+            maxArea=Math.max(maxArea,height*width);
         }
-        if(s.isEmpty()){
-            nsr[i]=heights.length;
-        }else{
-            nsr[i]=s.peek();
-        }
-        s.push(i);
+        st.push(i);
       }
-       s=new Stack<>();
-      for(int i=0;i<heights.length;i++){
-        while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
-            s.pop();
-        }if(s.isEmpty()){
-            nsl[i]=-1;
-        }else{
-            nsl[i]=s.peek();
-        }
-        s.push(i);
-      }
+      return maxArea;
 
-      for(int i=0;i<heights.length;i++){
-        int height=heights[i];
-        int width=nsr[i]-nsl[i]-1;
-        int currArea=height * width;
-        MaxArea=Math.max(currArea,MaxArea);
 
-      }
-    return MaxArea;
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//       int MaxArea=0;
+//       int nsr[]=new int[heights.length];
+//       int nsl[]=new int[heights.length];
+//       Stack<Integer> s=new Stack<>();
+//       for(int i=heights.length-1;i>=0;i--){
+//         while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
+//             s.pop();
+//         }
+//         if(s.isEmpty()){
+//             nsr[i]=heights.length;
+//         }else{
+//             nsr[i]=s.peek();
+//         }
+//         s.push(i);
+//       }
+//        s=new Stack<>();
+//       for(int i=0;i<heights.length;i++){
+//         while(!s.isEmpty() && heights[s.peek()]>=heights[i]){
+//             s.pop();
+//         }if(s.isEmpty()){
+//             nsl[i]=-1;
+//         }else{
+//             nsl[i]=s.peek();
+//         }
+//         s.push(i);
+//       }
+
+//       for(int i=0;i<heights.length;i++){
+//         int height=heights[i];
+//         int width=nsr[i]-nsl[i]-1;
+//         int currArea=height * width;
+//         MaxArea=Math.max(currArea,MaxArea);
+
+//       }
+//     return MaxArea;
+//     }
+// }
