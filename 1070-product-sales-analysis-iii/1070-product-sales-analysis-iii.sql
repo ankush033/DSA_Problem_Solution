844f74a4defdcf40fdabf8acf566e2dc4dd09a17
Ankush Gupta <ankushgupta7634@gmail.com>
2026-08-11 09:30:58 +0530

    Time: 786 ms (39.95%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+select 
+s.product_id,
+f.first_year,
+s.quantity,
+s.price
+ from Sales s
+join (
+    select product_id,
+    min(year) as first_year
+    from Sales
+    group by product_id
+) f
+on s.product_id=f.product_id
+and s.year=f.first_year;
