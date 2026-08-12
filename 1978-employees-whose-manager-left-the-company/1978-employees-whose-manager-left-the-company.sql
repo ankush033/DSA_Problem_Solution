7490b69542249b0abf3ba655c3540a3d88a6bd33
Ankush Gupta <ankushgupta7634@gmail.com>
2026-08-12 14:42:38 +0530

    Time: 841 ms (5.02%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -4,7 +4,7 @@ from Employees e
 where e.salary<30000
 and e.manager_id is not null
 and not exists(
-    select employee_id from Employees m
+    select 1 from Employees m
     where m.employee_id=e.manager_id
 )
 order by e.employee_id;
