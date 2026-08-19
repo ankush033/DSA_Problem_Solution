686c6b4229741798f971e57d75340e86c5b41268
Ankush Gupta <ankushgupta7634@gmail.com>
2026-08-19 14:23:34 +0530

    Time: 38 ms (62.73%), Space: 45.6 MB (98.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,28 +1,47 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+     int n=nums.length;
+     int dp[]=new int[n];
+     Arrays.fill(dp,1);
+     int ans=1;
+     for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[j]<nums[i]){
 
-ArrayList<Integer> list=new ArrayList<>();
-for(int num : nums){
-    int left=0;
-    int right=list.size();
+            dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+        }
+          ans=Math.max(ans,dp[i]);
+  
+  
+     }
+
+   return ans;
+         }
+
+}
+// ArrayList<Integer> list=new ArrayList<>();
+// for(int num : nums){
+//     int left=0;
+//     int right=list.size();
  
-    while(left<right){
-           int mid=left +(right-left)/2;
-        if(list.get(mid)<num){
-            left=mid+1;
+//     while(left<right){
+//            int mid=left +(right-left)/2;
+//         if(list.get(mid)<num){
+//             left=mid+1;
 
-        }else{
-            right=mid;
-        }
-    }
-        if(list.size()==left){
-            list.add(num);
-        }else{
-            list.set(left,num);
-        }
+//         }else{
+//             right=mid;
+//         }
+//     }
+//         if(list.size()==left){
+//             list.add(num);
+//         }else{
+//             list.set(left,num);
+//         }
     
-}
-return list.size();
+// }
+// return list.size();
 
 
 
@@ -46,8 +65,7 @@ return list.size();
 
 
 
-    }
-}
+ 
 //         ArrayList<Integer> list=new ArrayList<>();
 //         for(int num : nums){
 //             int idx=Collections.binarySearch(list,num);
